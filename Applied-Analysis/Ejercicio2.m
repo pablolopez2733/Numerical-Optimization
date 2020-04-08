da0423ac3388d88a16a73ea51c8752b589b8cf53
Pablo López Landeros <pablo.lopez.landeros@hotmail.com>
2020-04-08 15:37:10 -0500

    Proyecto Final Terminado

--- Applied-Analysis/Ejercicio2.m
+++ Applied-Analysis/Ejercicio2.m
@@ -1,12 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Ejercicio 2
-%Dibujar direcciones en 2d. Sea f : R2 a R y el punto inicial x0 cerca 
-%de un minimo local x0. La matriz Hessiana en x0 sea simetrica y 
-%(semi-)definida positiva y se usa para definir el modelo cuadratico en x0. 
-%Escoge una region de confianza con delta > 0. 
-%Luego haga un plot (en dos dimensiones) que contiene:
-%la frontera de la region de confianza, 
-%algunos conjuntos de nivel en R2 del modelo cuadratico en la region de la confianza. 
+%Dibujar direcciones en 2d. Sea f : R2 →R y el punto inicial x0 cerca 
+%de un mínimo local x?. La matriz Hessiana en x0 sea simétrica y 
+%(semi-)de�?nida positiva y se usa para de�?nir el modelo cuadratico en x0. 
+%Escoge una región de con�?anza con ∆ > 0. 
+%Luego haga un plot (en dos dimensiones) que contiene la frontera de la 
+%región de con�?anza algunos conjuntos de nivel en R2 del modelo cuadrático 
+%en la región de la con�?anza. 
 %los tres direcciones Newton, Cauchy, dogleg. Para obtenerlas use sus 
 %funciones pDogLeg, pCauchy.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -16,22 +16,23 @@ clc
 format long
 %% 1. Definimos nuestras variables a utilizar
 delta = 1;
-x0 = [-0.3;-0.7];
+x0 = [-0.2;-0.8];
 f=@(x) (1 + ((x(1) + x(2) + 1).^2) * (19 - (14 * x(1)) + (3 * (x(1) .^2)) - 14*x(2) + (6 .* x(1).*x(2)) + (3 * (x(2).^2)))) .* ...
         (30 + ((2 * x(1) - 3 * x(2)).^2) .* (18 - 32 * x(1) + 12 * (x(1) .^2) + 48 * x(2) - (36 .* x(1).*x(2)) + (27 * (x(2).^2))));
 gk = apGrad(f,x0);
 Bk = apHess(f,x0);
 %% 2. Definimos puntos notables
-pN = (-1)*inv(Bk)*gk;
+A = inv(Bk);
+pN = (-1)*A*gk;
 pC = pCauchy(Bk,gk,delta);
 pDL = pDogLeg(Bk,gk,delta);
-%% 3.Utilizamos el codigo scrLevel set en el modelo cuadrático
+%% 3.Utilizamos el código scrLevel set en el modelo cuadrático
 mk = @(x,y) f(x0)+(gk(1)).*x+(gk(2)).*y+(1/2)*((Bk(1,1)).*(x.^2)+((Bk(1,2))+(Bk(2,1))).*x.*y+(Bk(2,2)).*(y.^2));
 stepsize =  0.01;  
 [X,Y] = meshgrid(-2:stepsize:2);
 Z = mk(X,Y);
-niveles = [-5,0.01:5];
-contour(X,Y,Z,niveles);
+%niveles = [-3,0.01:3];
+contour(X,Y,Z,40);
 
 %% 4. Necesitamos definir una circunferencia
 T = linspace(0, 2*pi, 128);
@@ -41,11 +42,11 @@ axis equal
 %% 5. Graficamos
 hold on
 
-dirN = quiver(x0(1), x0(2),pN(1), pN(2),'Color', 'red',   'LineWidth', 2.5);
+dirN = quiver(x0(1), x0(2), pN(1), pN(2),'Color', 'red', 'LineWidth', 2);
 
-dirC = quiver(x0(1), x0(2), pC(1), pC(2),'Color', 'blue',   'LineWidth', 2.5);
+dirC = quiver(x0(1), x0(2), pC(1), pC(2),'Color', 'blue',   'LineWidth', 2);
 
-dirDog = quiver(x0(1), x0(2), pDL(1), pDL(2),'Color', 'green', 'LineWidth', 2.5);
+dirDog = quiver(x0(1), x0(2), pDL(1), pDL(2),'Color', 'green', 'LineWidth', 2);
 
 regionConfianza = plot(xpolar, ypolar,'r--', 'LineWidth',1);
 
